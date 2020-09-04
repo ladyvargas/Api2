4681e3106f438e80241b078eba3bcd3c846238e8
ladyvargas <45071201+ladyvargas@users.noreply.github.com>
2020-09-04 11:34:27 -0500

    listapedidos

--- models/pedidos.js
+++ models/pedidos.js
@@ -2,12 +2,18 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Pedidos= Schema({
-        Id_Producto:String,
-        name_producto: String,
-        description_producto:String,
-        precio_producto:String
+        id_cliente : String,
+        Nombre : String,
+        Pedidos : [
+            {
+                Id_Producto : String,
+                name_producto : String,
+                description_producto : String,
+                precio_producto : String
+            }
+        ]
     },
-    {collection: "listpedidos"}
+    {collection: "pedidos"}
 );
 
 module.exports = mongoose.model('Pedidos', Pedidos)
